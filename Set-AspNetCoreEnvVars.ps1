f74384a8219c2d19e058aae95ff4cc438b73f806
Benjamin Tam <benjamin.tam@readify.net>
2017-08-24 22:12:20 +1000

    Basic script works

--- Set-AspNetCoreEnvVars.ps1
+++ Set-AspNetCoreEnvVars.ps1
@@ -1,15 +1,36 @@
-$webConfig = Resolve-Path 'web.config'
-if (!(Test-Path $webConfig)) {
-    Write-Host "Could not find $webConfig"
+Param(
+    [Parameter(Mandatory=$True, Position=1)]
+    [ValidateScript({Test-Path $_ -PathType Leaf})]
+    [ValidatePattern("(?i)(web\.config)$")]
+    [string]$WebConfig,
+
+    [Parameter(Mandatory=$True, Position=2)]
+    [string]$ParameterName,
+
+    [Parameter(Mandatory=$True, Position=3)]
+    [string]$ParameterValue
+)
+
+$xml = (Get-Content $WebConfig) -as [Xml]
+$environmentVariables = $xml.configuration.'system.webServer'.aspNetCore.environmentVariables
+$environmentVariable = $environmentVariables.environmentVariable | Where-Object {$_.name -eq $ParameterName}
+if ($environmentVariable) {
+    $environmentVariable.value = $ParameterValue
+}
+elseif ($environmentVariables) {
+    $environmentVariable = $xml.CreateElement("environmentVariable");
+    $environmentVariable.SetAttribute("Name", $ParameterName);
+    $environmentVariable.SetAttribute("Value", $ParameterValue);
+    $environmentVariables.AppendChild($environmentVariable) 1>$null
+}
+else {
+    Write-Error "Could not find 'configuration/system.webServer/aspNetCore/environmentVariables' element in web.config"
     Exit 1
 }
-$doc = (Get-Content $webConfig) -as [Xml]
-$obj = $doc.configuration.'system.webServer'.aspNetCore.environmentVariables.environmentVariable | Where-Object {$_.name -eq 'ASPNETCORE_ENVIRONMENT'}
 try {
-    $obj.value = 'Development'
-    $doc.Save($webConfig)    
+    $xml.Save((Resolve-Path $WebConfig))
 }
 catch {
-    Write-Host "Could not find ASPNETCORE_ENVIRONMENT element in web.config"
+    Write-Error "Could not save web.config because: $_.Exception.Message"
     Exit 1
 }
